365cc243d21e44a65b26756592d4b83d5ab29fdb
oneyuci <sunychuks223@gmail.com>
2024-10-02 12:15:37 +0100

    first commit

---
+++ lesson_4.py
@@ -0,0 +1,9 @@
+def jurgen_func():
+    var_1 = "Sunday Uchechukwu"
+    var_2 = "Sunday Destiny"
+    var_3 = 7
+    print(var_1, var_2, var_3)
+    
+# This is my first function
+jurgen_func()
+
---
+++ sunday_uchechukwu.py
@@ -0,0 +1,20 @@
+# This is the function line
+def sunday_uchechukwu_func():
+
+    # This is the variale_name line 
+    name = "Sunday Uchechukwu"
+
+    # This is the variable_height line
+    height = 11.7
+
+    # This is the variable_age line
+    age = 00
+
+    # This is the variable_gender
+    is_male = True
+
+    # This is the print function
+    print(name, height, age, is_male)
+    
+# This is the call function
+sunday_uchechukwu_func()
