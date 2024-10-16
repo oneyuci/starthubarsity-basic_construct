bdfb9ea771bd1c1072bf751663a9a9453087250f
oneyuci <sunychuks223@gmail.com>
2024-10-16 12:45:19 +0100

    my commit

---
+++ lesson_10.py
@@ -0,0 +1,16 @@
+array = [4,7,3,9,22]
+sum = array[2] + array[4]
+print(sum)
+
+array.append(6)
+print(array)
+
+array.remove(22)
+print(array)
+
+array.pop(3)
+print(array)
+
+del array[3]
+
+print(array)
---
+++ lesson_11.py
@@ -0,0 +1,10 @@
+my_tuple = (1, 2, 3)
+my_tuple = (3,4,5)
+
+print(my_tuple)
+
+number = {"joy", "samuel", "joy", "amos"}
+print(number)
+
+number2 = {10, 4, 5, 3, 10, 2, 4}
+print(number2)
